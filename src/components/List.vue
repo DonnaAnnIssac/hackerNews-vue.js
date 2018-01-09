7cab4653d152343426edb1fd542190b8c4edcf91
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-09 16:38:47 +0530

    Add child component to display list of posts

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="post in posts">
+        <div class="card">
+            <div>{{ post.score }}</div>
+            <div>
+                <div> <strong>{{ post.title }}</strong> </div>
+                <div>
+                    <div> by {{ post.by }} </div>
+                    <div> {{ post.time }} </div>
+                    <div> {{ post.descendants}} comments </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+      props: ['posts']
+  }
+</script>
