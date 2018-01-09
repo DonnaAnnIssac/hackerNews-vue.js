871b413bd354a661a88a20d1c6fe9fd3092d69fe
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-09 11:49:30 +0530

    Add paths to child components

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Top from '@/components/Top'
+import New from '@/components/New'
+import Ask from '@/components/Ask'
+import Show from '@/components/Show'
+import Jobs from '@/components/Jobs'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const routes = [
+  {
+    path: '/',
+    name: 'Top',
+    component: Top
+  },
+  {
+    path: '/new',
+    name: 'New',
+    component: New
+  },
+  {
+    path: '/show',
+    name: 'Show',
+    component: Show
+  },
+  {
+    path: '/ask',
+    name: 'Ask',
+    component: Ask
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
+  }
+]
+
+const router = new Router({
+  routes,
+  mode: 'history'
 })
+
+export default {router, routes}
