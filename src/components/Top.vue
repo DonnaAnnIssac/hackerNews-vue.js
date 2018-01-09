d3b7583c29a72cfd46a09ce50b22eb90d1f9a18c
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-09 15:44:35 +0530

    Add methods to fetch data from API and pass data to child component as props

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,34 +1,49 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h2><top-list v-bind:posts="stories"></top-list></h2>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
+import topList from './TopList.vue'
+
+export default ({
+  name: 'Top',
+  components: { 'top-list': topList },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      results : [],
+      stories : []
+    }
+  },
+  methods : {
+    getPosts() {
+      for (let i = 0; i < 20; i++) {
+        let xhr = new XMLHttpRequest()
+        xhr.open('GET', 'https://hacker-news.firebaseio.com/v0/item/' + this.results[i] + '.json', true)
+        xhr.send()
+        xhr.onreadystatechange = () => {
+          if (xhr.status === 200 && xhr.readyState === 4) {
+            this.stories.push(JSON.parse(xhr.responseText))
+          }
+        }
+      }
+    }
+  },
+  mounted() {
+    let xhr = new XMLHttpRequest()
+    xhr.open('GET', 'https://hacker-news.firebaseio.com/v0/topstories.json', true)
+    xhr.send()
+    xhr.onreadystatechange = () => {
+      if (xhr.status === 200 && xhr.readyState === 4) {
+        this.results = JSON.parse(xhr.responseText)
+        this.getPosts()
+      }
     }
   }
-}
+
+})
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
