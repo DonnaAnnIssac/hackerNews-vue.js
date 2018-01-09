5e979a5df9bd695641ce6e3e28b5a56ccb1dd3cc
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-09 15:40:09 +0530

    import router file

--- src/main.js
+++ src/main.js
@@ -2,14 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import VueRouter from './router'
-
-const router = VueRouter.router
-const routes = VueRouter.routes
+import router from './router'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
