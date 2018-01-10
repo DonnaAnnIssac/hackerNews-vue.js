61f43a867d47ee471f9112fddb2f96a5b65c1ea1
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-10 20:38:09 +0530

    Add post component to display post info

---
+++ src/components/Post.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main">
+        <div class="card">
+            <div><strong>{{this.postinfo.title}}</strong></div>
+            <div>{{this.postinfo.score}}</div>
+            <div> by {{this.postinfo.by}} </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['postinfo'],
+    mounted() {
+      console.log('Mounted ' + this.postinfo)
+    }
+}
+</script>
