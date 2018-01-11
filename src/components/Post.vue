1520c998c34b30b272fb9cc967a95247c5c1aaa6
Donna Ann Issac <donna.ann1209@gmail.com>
2018-01-11 12:06:21 +0530

    Change structure of template

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,18 +1,50 @@
 <template>
     <div class="main">
         <div class="card">
-            <div><strong>{{this.postinfo.title}}</strong></div>
-            <div>{{this.postinfo.score}}</div>
-            <div> by {{this.postinfo.by}} </div>
+          <div>{{postObj.title}}</div>
+          <div>
+            <div>{{postObj.score}}</div>
+            <div>by {{this.postObj.by}}</div>
+            <div>{{this.timeAgo(this.postObj.time)}}</div>
+          </div>
+        </div>
+        <div class="card">
+
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['postinfo'],
-    mounted() {
-      console.log('Mounted ' + this.postinfo)
+    props: ['id'],
+    data() {
+      return {
+        postObj: {}
+      }
+    },
+    methods: {
+      timeAgo(time) {
+        let ts = new Date(time)
+        let nowTs = Math.floor(new Date().getTime()/1000)
+        let seconds = nowTs - ts
+        if (seconds > 2 * 24 * 3600) return Math.floor(seconds/(2 * 24 * 3600)) + " days ago"
+        if ( seconds > 24 * 3600) return "yesterday"
+        if (seconds > 2 * 3600) return Math.floor(seconds/3600) + " hours ago"
+        if (seconds > 3600) return "1 hour ago"
+        if (seconds > 60) return Math.floor(seconds/60) + " minutes ago"
+        if (seconds === 60) return "1 minute ago"
+        return seconds + " seconds ago"
+      }
+    },
+    created() {
+      let xhr = new XMLHttpRequest()
+      xhr.open('GET', 'https://hacker-news.firebaseio.com/v0/item/' + this.id + '.json', true)
+      xhr.send()
+      xhr.onreadystatechange = () => {
+      if (xhr.status === 200 && xhr.readyState === 4) {
+          this.postObj = JSON.parse(xhr.responseText)
+        }
+      }
     }
 }
 </script>
